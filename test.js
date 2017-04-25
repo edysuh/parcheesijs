257b300b9d71cded8c56bf36d88cf7e3dc1de575
edysuh <edys@u.northwestern.edu>
2017-04-24 20:48:17 -0500

    implemented proper rules checker; only bopping and doubles left

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import { board_test } from "./tests/board_test";
 import { move_test } from "./tests/move_test";
 import { moveMain_test } from "./tests/moveMain_test";
 import { enterPiece_test } from "./tests/enterPiece_test";
+import { rulesChecker_test } from "./tests/rulesChecker_test";
 
 function test() {
 	die_test();
@@ -10,6 +11,8 @@ function test() {
   move_test();
   moveMain_test();
 	enterPiece_test();
+	
+	rulesChecker_test();
 }
 
 test();
